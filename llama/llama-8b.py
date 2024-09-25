020ba98f3666c52a940bc3c78a5f97a630445209
paleumm <zdotqdot@gmail.com>
2024-09-25 11:28:45 +0700

    add token

--- llama/llama-8b.py
+++ llama/llama-8b.py
@@ -2,19 +2,22 @@ import ray
 from ray import serve
 from accelerate import init_empty_weights, infer_auto_device_map
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import os
+
+HF_TOKEN = os.getenv("HF_TOKEN")
 
 # Define the Llama model class for Ray Serve
 @serve.deployment(num_replicas=2, ray_actor_options={"num_gpus": 1})
 class LlamaModel:
     def __init__(self):
         model_name = "meta-llama/Meta-Llama-3.1-8B"
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, token=HF_TOKEN)
 
         with init_empty_weights():
-            self.model = AutoModelForCausalLM.from_pretrained(model_name)
+            self.model = AutoModelForCausalLM.from_pretrained(model_name, token=HF_TOKEN)
 
         device_map = infer_auto_device_map(self.model, no_split_module_classes=["LlamaBlock"])
-        self.model = self.model.from_pretrained(model_name, device_map=device_map)
+        self.model = self.model.from_pretrained(model_name, device_map=device_map, token=HF_TOKEN)
 
     def __call__(self, request: dict) -> dict:
         input_text = request["text"]
